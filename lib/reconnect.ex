ef5a6f5c07bd903ae085868597f1bedff8bc2e20
eddy147 <eddy147@gmail.com>
2022-10-22 19:07:07 +0200

    first commit

---
+++ lib/reconnect.ex
@@ -0,0 +1,18 @@
+defmodule Reconnect do
+  @moduledoc """
+  Documentation for `Reconnect`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Reconnect.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reconnect_test.exs
@@ -0,0 +1,8 @@
+defmodule ReconnectTest do
+  use ExUnit.Case
+  doctest Reconnect
+
+  test "greets the world" do
+    assert Reconnect.hello() == :world
+  end
+end
